a7aca023f73b33ee2971da2e479a5a568d601fe3
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-03 14:22:26 +0200

    Fixed a typo (email_box_sll -> email_box_ssl) and allowed the slug to be blank, after which it will now be auto generated when saved.

--- tracker/models.py
+++ tracker/models.py
@@ -8,7 +8,7 @@ class Queue(models.Model):
 	"""
 	
 	title = models.CharField('title', max_length=250)
-	slug = models.SlugField('slug', help_text='A unique name for this queue. If empty this is generated from the title.')
+	slug = models.SlugField('slug', blank=True, help_text='A unique name for this queue. If empty this is generated from the title.')
 	email_address = models.EmailField('e-mail address', blank=True, null=True, help_text='All outgoing e-mails for this queue will be sent from this address. If you plan on importing e-mails from a POP3 or IMAP mailbox, the e-mails will be imported from this e-mailaddress.')
 	allow_public_submission = models.BooleanField('allow public submission?', default=True, blank=True, help_text='Allow tickets from non logged-in users?')
 	allow_email_submission = models.BooleanField('allow e-mail submission?', default=False, blank=True, help_text='Allow the creation of tickets from e-mails?')
@@ -19,7 +19,7 @@ class Queue(models.Model):
 	email_box_type = models.CharField('e-mail box type', max_length=5, choices=(('imap', 'IMAP'), ('pop3', 'POP3')), blank=True, null=True, help_text='E-mail server type for the mailbox used in auto ticket creation from e-mail.')
 	email_box_host = models.CharField('e-mail box host', max_length=250, blank=True, null=True, help_text='E-mail server host for the mailbox used in auto ticket creation from e-mail.')
 	email_box_port = models.IntegerField('e-mail box port', blank=True, null=True, help_text='E-mail server port for the mailbox used in auto ticket creation from e-mail. Default ports are 110 for POP3 and 143 for IMAP. Leave blank to use defaults.')
-	email_box_sll = models.BooleanField('use SSL for e-mail?', default=False, blank=True, help_text='Use SSL for connection to the e-mail server? Default ports are 995 for POP3 and 993 for IMAP when using SSL.')
+	email_box_ssl = models.BooleanField('use SSL for e-mail?', default=False, blank=True, help_text='Use SSL for connection to the e-mail server? Default ports are 995 for POP3 and 993 for IMAP when using SSL.')
 	email_box_user = models.CharField('e-mail username', max_length=200, blank=True, null=True, help_text='The username used for the connection to the mailbox.')
 	email_box_password = models.CharField('e-mail password', max_length=200, blank=True, null=True, help_text='The password used for the connection to the mailbox.')
 	email_box_imap_folder = models.CharField('IMAP folder', max_length=100, blank=True, null=True, help_text='When connection to an IMAP server, the name of the mail folder to process email from. Default is INBOX.')
