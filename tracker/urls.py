4dc01f843796448be62573ac19af546ae36cd90b
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-06 18:21:25 +0200

    New tickets can be created from the homepage and saved in the database, there is also a view for viewing the tickets.

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 	url(r'^$', 'tracker.views.public.index', name='tracker-public-index'),
+	url(r'^ticket/(?P<queue>\w+)/(?P<id>\d+)/$', 'tracker.views.public.view_ticket', name='tracker-public-view-ticket'),
 )
--- tracker/views/public.py
+++ tracker/views/public.py
@@ -1,6 +1,8 @@
 from tracker.models import Queue, Ticket
 from tracker.forms import PublicTicketForm
 from tracker.lib import response
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
 	if request.user.is_staff:
@@ -8,10 +10,31 @@ def index(request):
 		pass
 	
 	if request.method == "POST":
-		# saving and creating the new ticket
-		pass
+		form = PublicTicketForm(request.POST, request.FILES)
+		form.fields['queue'].choices = [[q.id, q.title] for q in Queue.objects.filter(allow_public_submission=True)]
+		if form.is_valid():
+			ticket = form.save()
+			
+			return HttpResponseRedirect('%s?email=%s' % (reverse('tracker-public-view-ticket', args=[ticket.queue.slug, ticket.id]), ticket.submitter_email))
+		
 	else:
 		form = PublicTicketForm()
 		form.fields['queue'].choices = [[q.id, q.title] for q in Queue.objects.filter(allow_public_submission=True)]
 		
-		return response(request, 'tracker/public/index.html', locals())
+	return response(request, 'tracker/public/index.html', locals())
+	
+def view_ticket(request, queue, id):
+	email = request.GET.get('email', '')
+	error_message = ''
+	
+	if email:
+		try:
+			ticket = Ticket.objects.get(pk=id, queue__slug__iexact=queue, submitter_email__iexact=email)
+		except Ticket.DoesNotExist:
+			ticket = False
+			error_message = 'Invalid ticket ID or e-mail address. Please try again.'
+			
+		if ticket:
+			return response(request, 'tracker/public/view_ticket.html', locals())
+	
+	return response(request, 'tracker/public/404.html', locals())
