c444402e7e56a65a017a15f8c63354137390d93f
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-07 20:09:59 +0200

    Added a refactored form of the Ticket model and the TicketAdmin model.

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from tracker.models import Queue
+from tracker.models import Queue, Ticket
 
 class QueueAdmin(admin.ModelAdmin):
 	fieldsets = (
@@ -10,5 +10,29 @@ class QueueAdmin(admin.ModelAdmin):
 	list_filter = ('active',)
 	list_per_page = 25
 	search_fields = ['title', 'email_address', 'new_ticket_cc']
+	actions = ['deactivate_queues', 'activate_queues']
+	
+	def deactivate_queues(self, request, queryset):
+		queryset.update(active=False)
+	deactivate_queues.short_description = 'Mark selected queues as inactive'
+	
+	def activate_queues(self, request, queryset):
+		queryset.update(active=True)
+	activate_queues.short_description = 'Mark selected queues as active'
+	
+class TicketAdmin(admin.ModelAdmin):
+	fieldsets = (
+		('General information', {'fields': ('queue', 'title', 'description', 'public', 'submitter_email'), 'classes': ('wide', 'extrapretty')}),
+		('Advanced informtion', {'fields': ('status', 'priority', 'on_hold', 'assigned_to'), 'classes': ('wide', 'extrapretty')}),
+		('Resolution', {'fields': ('resolution',), 'classes': ('wide', 'extrapretty')}),
+	)
+	list_display = ('title', 'id', 'queue', 'public', 'submitter_email', 'status', 'priority', 'on_hold', '_get_assigned_to')
+	list_display_links = ('title',)
+	list_per_page = 25
+	list_filter = ('modified', 'status', 'priority', 'on_hold', 'queue')
+	search_fields = ['title', 'submitter_email', 'assigned_to__first_name', 'assigned_to__last_name', 'description']
+	date_hierarchy = 'created'
+	raw_id_fields = ['assigned_to']
 
 admin.site.register(Queue, QueueAdmin)
+admin.site.register(Ticket, TicketAdmin)
--- tracker/models.py
+++ tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from tracker.lib import unique_slugify
 
 class Queue(models.Model):
@@ -23,3 +24,74 @@ class Queue(models.Model):
 			unique_slugify(self, self.title, 'slug')
 			
 		super(Queue, self).save(*args, **kwargs)
+		
+class Ticket(models.Model):
+	NEW_STATUS = 1
+	OPEN_STATUS = 2
+	REOPENED_STATUS = 3
+	RESOLVED_STATUS = 4
+	CLOSED_STATUS = 5
+	
+	STATUS_CHOICES = (
+		(NEW_STATUS, 'New'),
+		(OPEN_STATUS, 'Open'),
+		(REOPENED_STATUS, 'Reopened'),
+		(RESOLVED_STATUS, 'Resolved'),
+		(CLOSED_STATUS, 'Closed'),
+	)
+	
+	PRIORITY_CHOICES = (
+		(1, '1. Critical'),
+		(2, '2. High'),
+		(3, '3. Normal'),
+		(4, '4. Low'),
+		(5, '5. Very low'),
+	)
+	
+	queue = models.ForeignKey(Queue)
+	title = models.CharField('Title', max_length=250)
+	description = models.TextField('Description', blank=True)
+	resolution = models.TextField('Resolution', blank=True)
+	public = models.BooleanField('Public ticket?', default=True)
+	submitter_email = models.EmailField('Submitter e-mail', blank=True)
+	assigned_to = models.ForeignKey(User, verbose_name='Assigned to', blank=True)
+	status = models.IntegerField('Status', choices=STATUS_CHOICES, default=NEW_STATUS)
+	on_hold = models.BooleanField('On hold?', default=False)
+	priority = models.IntegerField('Priority', choices=PRIORITY_CHOICES, default=3)
+	last_escalation = models.DateTimeField('Last escalation date', blank=True, editable=False, null=True)
+	created = models.DateTimeField('Created', auto_now_add=True)
+	modified = models.DateTimeField('Modified', auto_now=True)
+	
+	def _get_assigned_to(self):
+		if not self.assigned_to:
+			return 'Unassigned'
+		else:
+			if self.assigned_to.get_full_name():
+				return self.assigned_to.get_full_name()
+			else:
+				return self.assigned_to
+	_get_assigned_to.short_description = 'Assigned to'
+	get_assigned_to = property(_get_assigned_to)
+	
+	def _get_ticket_slug(self):
+		return "[%s - %s]" % (self.queue.slug, self.id)
+	get_ticket_slug = property(_get_ticket_slug)
+	
+	def _get_status(self):
+		held_msg = ''
+		if self.on_hold:
+			held_msg = ' - on hold'
+		
+		return "%s%s" % (self.get_status_display().lower(), held_msg)
+	get_status = property(_get_status)
+	
+	def __unicode__(self):
+		return self.title
+		
+	class Meta:
+		get_latest_by = 'created'
+		ordering = ['-created']
+		
+	@models.permalink
+	def get_absolute_url(self):
+		pass
