2d6befa9235ecbe2d0661af6b548f2f196759904
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-07 23:12:33 +0200

    For instance sounds much much better then like.

--- tracker/forms.py
+++ tracker/forms.py
@@ -8,7 +8,7 @@ class PublicTicketForm(forms.Form):
 	submitter_email = forms.EmailField(label='Your e-mail address', required=True, help_text='This e-mail address will be used to inform you of all updates made to this ticket.')
 	description = forms.CharField(label='Description of your issue', required=True, widget=forms.Textarea(), help_text='Please provide as much details as possible. This will help us solve your issue much faster.')
 	priority = forms.ChoiceField(label='Priority', choices=Ticket.PRIORITY_CHOICES, initial=3, help_text='Please select the priority carefully. Priorities can and will be changed by members of the staff when reviewing your issue.')
-	attachment = forms.FileField(label='Attach a file', required=False, help_text='You can attach a file (like a screenshot) to clarify your issue.')
+	attachment = forms.FileField(label='Attach a file', required=False, help_text='You can attach a file (for instance a screenshot) to clarify your issue.')
 	
 	def save(self):
 		t = Ticket(title=self.cleaned_data['title'], submitter_email=self.cleaned_data['submitter_email'], status=Ticket.NEW_STATUS, queue=self.cleaned_data['queue'], description=self.cleaned_data['description'], priority=self.cleaned_data['priority'])
