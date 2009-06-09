cbb9bd92fbfec8646331d7fca9961add71b2b84e
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-09 19:07:41 +0200

    Removed all files, version 2.0 will be a complete redesign from the ground up.

--- tracker/managers.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class FollowUpManager(models.Manager):
-	def private_followups(self):
-		return self.filter(public=False)
-	
-	def public_followups(self):
-		return self.filter(public=True)
--- tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('tracker.views.public',
-	url(r'^$', 'index', name='tracker-public-index'),
-	url(r'^ticket/find/$', 'find_ticket', name='tracker-public-find-ticket'),
-	url(r'^ticket/browse/$', 'browse_tickets', name='tracker-public-browse-tickets'),
-	url(r'^ticket/(?P<queue>\w+)/(?P<id>\d+)/$', 'view_ticket', name='tracker-public-view-ticket'),
-)
