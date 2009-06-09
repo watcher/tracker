c333c2e9ee53a6fe30f6592f3d8800c8bbe1d72e
Bernard Siebens <bernard.siebens@student.kuleuven.be>
2009-06-09 14:37:38 +0200

    Added the browse tickets page. Only public tickets will show up on this page.

--- tracker/views/public.py
+++ tracker/views/public.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from tracker.lib import response
 from tracker.forms import PublicTicketForm, FollowUpForm
-from tracker.models import Ticket
+from tracker.models import Ticket, Queue
 
 def index(request):
 	if request.method == "POST":
@@ -69,3 +69,11 @@ def find_ticket(request):
 		return HttpResponseRedirect('%s?email=%s' % (reverse('tracker-public-view-ticket', args=[ticket.queue.slug, ticket.id]), ticket.submitter_email))
 	else:
 		return response(request, 'tracker/public/find_ticket.html', {})
+		
+def browse_tickets(request):
+	tickets = []
+	
+	for q in Queue.objects.all():
+		tickets.append({'queue': q, 'tickets': q.ticket_set.filter(public=True).filter(status__gt=Ticket.NEW_STATUS).filter(status__lt=Ticket.CLOSED_STATUS)})
+		
+	return response(request, 'tracker/public/browse_tickets.html', {'tickets': tickets})
